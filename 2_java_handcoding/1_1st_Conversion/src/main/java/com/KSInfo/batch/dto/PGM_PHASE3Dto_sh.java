d85e44e7dfe97f182543091113b55423c50cd157
KS情報システム\KS情報システム株式会社 <duddl1016@naver.com>
2026-03-23 09:35:33 +0900

    feat : PHASE3Dto_sh

--- 2_java_handcoding/1_1st_Conversion/src/main/java/com/KSInfo/batch/dto/PGM_PHASE3Dto_sh.java
+++ 2_java_handcoding/1_1st_Conversion/src/main/java/com/KSInfo/batch/dto/PGM_PHASE3Dto_sh.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class PGM_PHASE3Dto{
+public class PGM_PHASE3Dto_sh{
     private WS_DB_CONN wS_DB_CONN;
     private WS_SYS_COMMON_AREA wS_SYS_COMMON_AREA;
     private ERR_LOG_AREA eRR_LOG_AREA;
