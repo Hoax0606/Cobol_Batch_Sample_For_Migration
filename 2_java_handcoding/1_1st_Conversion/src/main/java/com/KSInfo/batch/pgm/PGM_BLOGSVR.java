a47f7c41cc800343ae6dfb18bcc4071eb261d130
Suhyun-KS <piggy98911@gmail.com>
2026-03-30 15:35:32 +0900

    feat : Update BLOGSVR.java

--- 2_java_handcoding/1_1st_Conversion/src/main/java/com/KSInfo/batch/pgm/PGM_BLOGSVR.java
+++ 2_java_handcoding/1_1st_Conversion/src/main/java/com/KSInfo/batch/pgm/PGM_BLOGSVR.java
@@ -20,17 +20,17 @@ public class PGM_BLOGSVR {
     //!!1.PROCEDURE                      DIVISION USING DCL-TB-BATCH-LOG.
     
     private DCL_TB_BATCH_LOGDto DCL_TB_BATCH_LOGDto = new DCL_TB_BATCH_LOGDto();
+    private int SQLCODE = 0;
     
 
     public void MAIN(PGM_BLOGSVRDto dto){
         //!!2.PROCEDURE                      DIVISION USING DCL-TB-BATCH-LOG.
-        // DCL_TB_BATCH_LOGDto DCL_TB_BATCH_LOGDto = dto.getDCL_TB_BATCH_LOGDto();
 
         dto.getDCL_TB_BATCH_LOGDto().setBLG_RETURN_CODE(0);
 
-        DB_CONNECT_CHECK(dto);
+        DB_CONNECT(dto);
 
-        if(DCL_TB_BATCH_LOGDto.getBLG_RETURN_CODE() !=0) {
+        if(DCL_TB_BATCH_LOGDto.getBLG_RETURN_CODE() != 0) {
             return;
         } else {
             // CONTINUE
@@ -43,14 +43,12 @@ public class PGM_BLOGSVR {
             BATCHLOG_END(dto);
         } 
         else {
-            // log.warn("> [BLOGSVR] UNKNOWN ACTION: " + action);
             log.info("> [BLOGSVR] UNKNOWN ACTION: " + dto.getDCL_TB_BATCH_LOGDto().getBLG_ACTION());
             DCL_TB_BATCH_LOGDto.setBLG_RETURN_CODE(8);
         }
 
         if (DCL_TB_BATCH_LOGDto.getBLG_RETURN_CODE() == 0) {
             // !! EXEC SQL COMMIT            END-EXEC
-                // log.info("> [BLOGSVR] PROCESS SUCCESSFULLY COMMITTED.");
         } else {
              // CONTINUE
         }
@@ -60,7 +58,7 @@ public class PGM_BLOGSVR {
 
     }
 
-    private void DB_CONNECT_CHECK(PGM_BLOGSVRDto dto) {
+    private void DB_CONNECT(PGM_BLOGSVRDto dto) {
         // ACCEPT WS-DATASRC      FROM ENVIRONMENT 'GIXSQL_DB_CONN'.
         // ACCEPT WS-USER         FROM ENVIRONMENT 'GIXSQL_USER'.
         // ACCEPT WS-PWD          FROM ENVIRONMENT 'GIXSQL_PWD'.
@@ -74,88 +72,74 @@ public class PGM_BLOGSVR {
                 //     CONNECT :WS-USER IDENTIFIED BY :WS-PWD
                 //             USING :WS-DATASRC
                 // END-EXEC.
-                // IF SQLCODE = 0 THEN
-                //     CONTINUE
-                // ELSE
-                //     MOVE SQLCODE TO WS-SQLCODE-DISP
-                //     DISPLAY '> [BLOGSVR] DB CONNECT FAILED. SQLCODE='
-                //             WS-SQLCODE-DISP
-                //     MOVE 8 TO BLG-RETURN-CODE
-                // END-IF
+                
+                if (SQLCODE == 0) {
+                    // CONTINUE
+                } else {
+                    dto.getWS_WORK_AREASDto().setWS_SQLCODE_DISP(SQLCODE);
+                    log.info("> [BLOGSVR] DB CONNECT FAILED. SQLCODE=" + dto.getWS_WORK_AREASDto().getWS_SQLCODE_DISP());
+                    dto.getDCL_TB_BATCH_LOGDto().setBLG_RETURN_CODE(8);
+                }
         }
 
     }
 
     private void BATCHLOG_START(PGM_BLOGSVRDto dto) {
-        // dto.getDCL_TB_BATCH_LOGDto.setBLG_START_DT(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
         DCL_TB_BATCH_LOGDto.setBLG_START_DT(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
         DCL_TB_BATCH_LOGDto.setBLG_END_DT("");
         DCL_TB_BATCH_LOGDto.setBLG_STAT("R");
         DCL_TB_BATCH_LOGDto.setBLG_PROC_CNT(0);
         DCL_TB_BATCH_LOGDto.setBLG_ERR_CNT(0);
 
-        // EXEC SQL
-        //     INSERT INTO TB_BATCH_LOG
-        //             (PGM_ID,        BIZ_DATE,      START_DT,
-        //             END_DT,        STAT,          PROC_CNT,
-        //             ERR_CNT,       REMARK)
-        //     VALUES (:BLG-PGM-ID,   :BLG-BIZ-DATE, :BLG-START-DT,
-        //             :BLG-END-DT,   :BLG-STAT,     :BLG-PROC-CNT,
-        //             :BLG-ERR-CNT,  :BLG-REMARK)
-        // END-EXEC.
-        int rs1 = dao.insert_01(dto);
+        try {
+            dao.insert_01(dto);
 
-        // IF SQLCODE NOT = 0 THEN
-        //    MOVE SQLCODE TO WS-SQLCODE-DISP
-        //    DISPLAY '> [BLOGSVR] START INSERT ERROR. SQLCODE='
-        //            WS-SQLCODE-DISP
-        //    MOVE 8 TO BLG-RETURN-CODE
-        // ELSE
-        //    EXEC SQL COMMIT            END-EXEC
+            if (SQLCODE != 0) {
+                dto.getWS_WORK_AREASDto().setWS_SQLCODE_DISP(SQLCODE);
+                log.info("> [BLOGSVR] START INSERT ERROR. SQLCODE=" + dto.getWS_WORK_AREASDto().getWS_SQLCODE_DISP());
+                dto.getDCL_TB_BATCH_LOGDto().setBLG_RETURN_CODE(8);
+            } else {
+                // EXEC SQL COMMIT            END-EXEC
 
-        //    EXEC SQL
-        //        SELECT MAX(BATCH_ID)
-        //          INTO :BLG-BATCH-ID
-        //          FROM TB_BATCH_LOG
-        //         WHERE PGM_ID   = :BLG-PGM-ID
-        //           AND BIZ_DATE = :BLG-BIZ-DATE
-        //    END-EXEC
-        dao.select_01(dto);
+                try {
+                    dao.select_01(dto);
 
-        //    IF SQLCODE NOT = 0 THEN
-        //        MOVE SQLCODE TO WS-SQLCODE-DISP
-        //        DISPLAY '> [BLOGSVR] BATCH_ID SELECT ERROR. SQLCODE='
-        //                WS-SQLCODE-DISP
-        //        MOVE 8 TO BLG-RETURN-CODE
-        //    ELSE
-        //        CONTINUE
-        //    END-IF
-        // END-IF.
+                    if (SQLCODE != 0) {
+                        dto.getWS_WORK_AREASDto().setWS_SQLCODE_DISP(SQLCODE);
+                        log.info("> [BLOGSVR] BATCH_ID SELECT ERROR. SQLCODE=" + dto.getWS_WORK_AREASDto().getWS_SQLCODE_DISP());
+                        dto.getDCL_TB_BATCH_LOGDto().setBLG_RETURN_CODE(8);
+                    } else {
+                        // CONTINUE
+                    }
+                } catch (Exception e) {
+
+                }
+            }
+        } catch (Exception e) {
+            
+        }
+
+        
+
     }
 
     private void BATCHLOG_END(PGM_BLOGSVRDto dto) {
         DCL_TB_BATCH_LOGDto.setBLG_END_DT(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss")));
-
-        // EXEC SQL
-        //     UPDATE TB_BATCH_LOG
-        //         SET END_DT   = :BLG-END-DT,
-        //             STAT     = :BLG-STAT,
-        //             PROC_CNT = :BLG-PROC-CNT,
-        //             ERR_CNT  = :BLG-ERR-CNT,
-        //             REMARK   = :BLG-REMARK
-        //     WHERE BATCH_ID = :BLG-BATCH-ID
-        // END-EXEC.
         
-        int rs = dao.update_01(dto);
+        try {
+            dao.update_01(dto);
 
-        // IF SQLCODE NOT = 0 THEN
-        //     MOVE SQLCODE TO WS-SQLCODE-DISP
-        //     DISPLAY '> [BLOGSVR] END UPDATE ERROR. SQLCODE='
-        //             WS-SQLCODE-DISP
-        //     MOVE 8 TO BLG-RETURN-CODE
-        // ELSE
-        //     CONTINUE
-        // END-IF.
+            if (SQLCODE != 0) {
+                dto.getWS_WORK_AREASDto().setWS_SQLCODE_DISP(SQLCODE);
+                log.info("> [BLOGSVR] END UPDATE ERROR. SQLCODE=" + dto.getWS_WORK_AREASDto().getWS_SQLCODE_DISP());
+                dto.getDCL_TB_BATCH_LOGDto().setBLG_RETURN_CODE(8);
+            } else {
+                // CONTINUE
+            }
+        } catch (Exception e) {
+            
+        }
+        
     }
 
 }
